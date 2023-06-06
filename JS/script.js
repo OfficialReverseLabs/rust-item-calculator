1e8075b65dfc51f9c098dafed6161c25069b5975
FaxHack™ <56932944+FaxHack@users.noreply.github.com>
2023-06-06 15:42:40 +0100

    added stuff

--- JS/script.js
+++ JS/script.js
@@ -1,50 +1,83 @@
-document.getElementById('calculate-btn').addEventListener('click', function() {
-    var rockets = parseInt(document.getElementById('rockets-input').value) || 0;
-    var hv = parseInt(document.getElementById('hv-input').value) || 0;
-    var flaming = parseInt(document.getElementById('flaming-input').value) || 0;
-    var c4 = parseInt(document.getElementById('c4-input').value) || 0;
-    var satchel = parseInt(document.getElementById('satchel-input').value) || 0;
-  
-    // Reset outputs
-    document.querySelectorAll('.output').forEach(function(output) {
-      output.textContent = '';
-    });
-  
-    if (rockets === 0 && hv === 0 && flaming === 0 && c4 === 0 && satchel === 0) {
-      alert('Please enter a quantity for at least one item.');
-      return;
-    }
-  
-    if (rockets < 0 || hv < 0 || flaming < 0 || c4 < 0 || satchel < 0) {
-      alert('Invalid input. Quantity cannot be negative.');
-      return;
-    }
-  
-    document.getElementById('rockets-sulfur-output').textContent = rockets * 1400;
-    document.getElementById('rockets-charcoal-output').textContent = rockets * 1950;
-    document.getElementById('rockets-metal-frags-output').textContent = rockets * 100;
-    document.getElementById('rockets-metal-pipes-output').textContent = rockets * 2;
-  
-    document.getElementById('hv-sulfur-output').textContent = hv * 1500;
-    document.getElementById('hv-charcoal-output').textContent = hv * 2100;
-    document.getElementById('hv-metal-frags-output').textContent = hv * 100;
-    document.getElementById('hv-metal-pipes-output').textContent = hv * 2;
-  
-    document.getElementById('flaming-sulfur-output').textContent = flaming * 610;
-    document.getElementById('flaming-charcoal-output').textContent = flaming * 900;
-    document.getElementById('flaming-metal-frags-output').textContent = flaming * 10;
-    document.getElementById('flaming-metal-pipes-output').textContent = flaming * 2;
-    
-    document.getElementById('c4-sulfur-output').textContent = c4 * 2200;
-    document.getElementById('c4-charcoal-output').textContent = c4 * 3000;
-    document.getElementById('c4-metal-frags-output').textContent = c4 * 200;
-    document.getElementById('c4-cloth-output').textContent = c4 * 5;
-    document.getElementById('c4-tech-trash-output').textContent = c4 * 2;
-  
-    document.getElementById('satchel-sulfur-output').textContent = satchel * 480;
-    document.getElementById('satchel-charcoal-output').textContent = satchel * 720;
-    document.getElementById('satchel-metal-frags-output').textContent = satchel * 80;
-    document.getElementById('satchel-cloth-output').textContent = satchel * 10;
-    document.getElementById('satchel-rope-output').textContent = satchel * 1;
+const calculateBtn = document.getElementById('calculate-btn');
+const rocketsInput = document.getElementById('rockets-input');
+const hvInput = document.getElementById('hv-input');
+const flamingInput = document.getElementById('flaming-input');
+const c4Input = document.getElementById('c4-input');
+const satchelInput = document.getElementById('satchel-input');
+
+calculateBtn.addEventListener('click', function() {
+  const rockets = parseInt(rocketsInput.value) || 0;
+  const hv = parseInt(hvInput.value) || 0;
+  const flaming = parseInt(flamingInput.value) || 0;
+  const c4 = parseInt(c4Input.value) || 0;
+  const satchel = parseInt(satchelInput.value) || 0;
+
+  // Reset outputs
+  document.querySelectorAll('.output').forEach(function(output) {
+    output.textContent = '';
   });
-  
+
+  if (rockets === 0 && hv === 0 && flaming === 0 && c4 === 0 && satchel === 0) {
+    alert('Please enter a quantity for at least one item.');
+    return;
+  }
+
+  if (rockets < 0 || hv < 0 || flaming < 0 || c4 < 0 || satchel < 0) {
+    alert('Invalid input. Quantity cannot be negative.');
+    return;
+  }
+
+  const rocketsSulfurOutput = rockets * 1400;
+  const rocketsCharcoalOutput = rockets * 1950;
+  const rocketsMetalFragsOutput = rockets * 100;
+  const rocketsMetalPipesOutput = rockets * 2;
+
+  const hvSulfurOutput = hv * 1500;
+  const hvCharcoalOutput = hv * 2100;
+  const hvMetalFragsOutput = hv * 100;
+  const hvMetalPipesOutput = hv * 2;
+
+  const flamingSulfurOutput = flaming * 610;
+  const flamingCharcoalOutput = flaming * 900;
+  const flamingMetalFragsOutput = flaming * 10;
+  const flamingMetalPipesOutput = flaming * 2;
+
+  const c4SulfurOutput = c4 * 2200;
+  const c4CharcoalOutput = c4 * 3000;
+  const c4MetalFragsOutput = c4 * 200;
+  const c4ClothOutput = c4 * 5;
+  const c4TechTrashOutput = c4 * 2;
+
+  const satchelSulfurOutput = satchel * 480;
+  const satchelCharcoalOutput = satchel * 720;
+  const satchelMetalFragsOutput = satchel * 80;
+  const satchelClothOutput = satchel * 10;
+  const satchelRopeOutput = satchel * 1;
+
+  document.getElementById('rockets-sulfur-output').textContent = rocketsSulfurOutput;
+  document.getElementById('rockets-charcoal-output').textContent = rocketsCharcoalOutput;
+  document.getElementById('rockets-metal-frags-output').textContent = rocketsMetalFragsOutput;
+  document.getElementById('rockets-metal-pipes-output').textContent = rocketsMetalPipesOutput;
+
+  document.getElementById('hv-sulfur-output').textContent = hvSulfurOutput;
+  document.getElementById('hv-charcoal-output').textContent = hvCharcoalOutput;
+  document.getElementById('hv-metal-frags-output').textContent = hvMetalFragsOutput;
+  document.getElementById('hv-metal-pipes-output').textContent = hvMetalPipesOutput;
+
+  document.getElementById('flaming-sulfur-output').textContent = flamingSulfurOutput;
+  document.getElementById('flaming-charcoal-output').textContent = flamingCharcoalOutput;
+  document.getElementById('flaming-metal-frags-output').textContent = flamingMetalFragsOutput;
+  document.getElementById('flaming-metal-pipes-output').textContent = flamingMetalPipesOutput;
+
+  document.getElementById('c4-sulfur-output').textContent = c4SulfurOutput;
+  document.getElementById('c4-charcoal-output').textContent = c4CharcoalOutput;
+  document.getElementById('c4-metal-frags-output').textContent = c4MetalFragsOutput;
+  document.getElementById('c4-cloth-output').textContent = c4ClothOutput;
+  document.getElementById('c4-tech-trash-output').textContent = c4TechTrashOutput;
+
+  document.getElementById('satchel-sulfur-output').textContent = satchelSulfurOutput;
+  document.getElementById('satchel-charcoal-output').textContent = satchelCharcoalOutput;
+  document.getElementById('satchel-metal-frags-output').textContent = satchelMetalFragsOutput;
+  document.getElementById('satchel-cloth-output').textContent = satchelClothOutput;
+  document.getElementById('satchel-rope-output').textContent = satchelRopeOutput;
+});
